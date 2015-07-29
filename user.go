e0b1a93fa1ca45ac147ae5cb4b9c1ceed85c9c8b
Geert-Johan Riemer <geertjohan@geertjohan.net>
2015-07-29 21:52:19 +0200

    Add JSON feed for users

--- user.go
+++ user.go
@@ -3,6 +3,7 @@ package guestbook
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -11,4 +12,24 @@ func userPage(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	userid := vars["id"]
 	fmt.Fprintf(w, "user %s", userid)
+}
+
+type jsonUser struct {
+	ID       uint64
+	Username string
+}
+
+func handleGetUser(r *http.Request) interface{} {
+	useridString := r.FormValue("ID")
+	userid, err := strconv.ParseUint(useridString, 10, 64)
+	if err != nil {
+		return nil
+	}
+
+	response := &jsonUser{
+		ID:       userid,
+		Username: "foobar",
+	}
+
+	return response
 }
